4901d8ff957c4328ec79ba73fc39d8f52a714572
farkhodakramov <farhodakramov01@gmail.com>
2025-03-27 15:36:32 +0500

    Commit

--- VideoPlayer/ViewController.swift
+++ VideoPlayer/ViewController.swift
@@ -2,147 +2,129 @@ import UIKit
 import AVKit
 import AVFoundation
 
-class CustomAVPlayerViewController: AVPlayerViewController {
-    var qualities: [(String, URL)] = []
-    var currentTime: CMTime = .zero
-}
-
-class ViewController: UIViewController {
-    @IBOutlet weak var platBtn: UIButton!
-    @IBOutlet weak var playerView: UIView!
-    @IBOutlet weak var tableView: UITableView!
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    private var player: AVPlayer!
+    private var playerViewController: AVPlayerViewController!
+    private var playerItem: AVPlayerItem!
+    private var qualityButton: UIButton!
+    private var videoTableView: UITableView!
     
-    let videoUrls = [
-        "https://devstreaming-cdn.apple.com/videos/streaming/examples/adv_dv_atmos/main.m3u8",
-        "https://devstreaming-cdn.apple.com/videos/streaming/examples/adv_dv_atmos/main.m3u8",
+    private let videoUrls = [
+        "https://cdn.uzd.udevs.io/uzdigital/videos/76cb1319e234a59764658c0c9d566d1e/master.m3u8",
         "https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8",
-        "https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8",
-        "https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8",
-        "https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8",
-        "https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8",
-        "https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8",
-        "https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8",
-        "https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8"
+        "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8",
+        "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
+      
+      
     ]
-    var qualities: [(String, URL)] = []
-    var customPlayerVC: CustomAVPlayerViewController?
-    var playingIndex: Int?
-
+    
+    private let availableQualities: [(String, Double)] = [
+        ("Auto", 0),
+        ("360p", 500_000),
+        ("480p", 1_000_000),
+        ("720p", 2_500_000),
+        ("1080p", 5_000_000)
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.register(UINib(nibName: "VideoPlayerTVC", bundle: nil), forCellReuseIdentifier: "VideoPlayerTVC")
-        playVideo(at: 0)  // Play the first video by default
-    }
-    
-    func playVideo(at index: Int) {
-        playingIndex = index
-        customPlayerVC?.currentTime = .zero
-        parseM3U8(urlString: videoUrls[index])
-        tableView.reloadData()
-    }
-    
-    func parseM3U8(urlString: String) {
-        guard let url = URL(string: urlString) else { return }
-        URLSession.shared.dataTask(with: url) { data, _, error in
-            guard let data = data, error == nil else { return }
-            if let playlist = String(data: data, encoding: .utf8) {
-                self.extractQualities(from: playlist, baseURL: url.deletingLastPathComponent())
-            }
-        }.resume()
-    }
-    
-    func extractQualities(from playlist: String, baseURL: URL) {
-        qualities.removeAll()
-        let lines = playlist.components(separatedBy: "\n")
-        for i in 0..<lines.count where lines[i].contains("RESOLUTION=") {
-            if let resolution = lines[i].components(separatedBy: "RESOLUTION=").last?.components(separatedBy: ",").first,
-               i + 1 < lines.count {
-                let videoURL = lines[i + 1]
-                if let qualityURL = URL(string: videoURL, relativeTo: baseURL) {
-                    qualities.append((resolution, qualityURL))
-                }
-            }
-        }
-        DispatchQueue.main.async {
-            self.addPlayerView(url: self.qualities.first?.1 ?? baseURL, resumeTime: self.customPlayerVC?.currentTime ?? .zero)
-        }
-    }
-    
-    func addPlayerView(url: URL, resumeTime: CMTime) {
-        customPlayerVC?.player?.pause()
-        customPlayerVC?.view.removeFromSuperview()
-        customPlayerVC?.removeFromParent()
         
-        let playerItem = AVPlayerItem(url: url)
-        let player = AVPlayer(playerItem: playerItem)
-        customPlayerVC = CustomAVPlayerViewController()
-        customPlayerVC!.qualities = qualities
-        addChild(customPlayerVC!)
-        customPlayerVC!.view.frame = playerView.bounds
-        playerView.addSubview(customPlayerVC!.view)
-        customPlayerVC!.didMove(toParent: self)
+        view.backgroundColor = .white
         
-        customPlayerVC!.player = player
-        player.seek(to: resumeTime)
+        setupPlayer()
+        setupQualityButton()
+        setupTableView()
+        
+        playVideo(url: videoUrls[0])
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(checkFullscreen), name: UIScreen.didConnectNotification, object: nil)
+    }
+    
+    private func setupPlayer() {
+        playerViewController = AVPlayerViewController()
+        playerViewController.view.frame = CGRect(x: 0, y: 50, width: view.bounds.width, height: view.bounds.height * 0.3)
+        playerViewController.view.backgroundColor = .black
+        addChild(playerViewController)
+        view.addSubview(playerViewController.view)
+        playerViewController.didMove(toParent: self)
+    }
+    
+    private func setupQualityButton() {
+        qualityButton = UIButton(type: .system)
+        qualityButton.setTitle("Change Quality", for: .normal)
+        qualityButton.setTitleColor(.systemBlue, for: .normal)
+        qualityButton.layer.cornerRadius = 10
+        qualityButton.addTarget(self, action: #selector(changeQuality), for: .touchUpInside)
+        
+        qualityButton.frame = CGRect(x: 20, y: playerViewController.view.frame.maxY - 60, width: 150, height: 40)
+        view.addSubview(qualityButton)
+    }
+    
+    private func setupTableView() {
+        videoTableView = UITableView()
+        videoTableView.delegate = self
+        videoTableView.dataSource = self
+        videoTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        videoTableView.frame = CGRect(x: 0, y: playerViewController.view.frame.maxY, width: view.bounds.width, height: view.bounds.height * 0.5)
+        
+        videoTableView.backgroundColor = .white
+        view.addSubview(videoTableView)
+    }
+    
+    private func playVideo(url: String) {
+        guard let videoURL = URL(string: url) else { return }
+        
+        let newPlayerItem = AVPlayerItem(url: videoURL)
+        
+        if player == nil {
+            player = AVPlayer(playerItem: newPlayerItem)
+            playerViewController.player = player
+        } else {
+            player.replaceCurrentItem(with: newPlayerItem)
+        }
+        
         player.play()
-        NotificationCenter.default.addObserver(self, selector: #selector(playerDidFinishPlaying), name: .AVPlayerItemDidPlayToEndTime, object: player.currentItem)
-        addQualityButton()
     }
     
-    @objc func playerDidFinishPlaying() {
-        guard let index = playingIndex else { return }
-        let cell = tableView.cellForRow(at: IndexPath(row: index, section: 0)) as? VideoPlayerTVC
-        cell?.lbl.textColor = .green  // Change text color when video finishes
-    }
     
-    func addQualityButton() {
-        let qualityButton = UIButton(type: .system)
-        qualityButton.setTitle("Quality", for: .normal)
-        qualityButton.addTarget(self, action: #selector(showQualityOptions), for: .touchUpInside)
-        qualityButton.translatesAutoresizingMaskIntoConstraints = false
-        playerView.addSubview(qualityButton)
-        NSLayoutConstraint.activate([
-            qualityButton.leadingAnchor.constraint(equalTo: playerView.leadingAnchor, constant: 16),
-            qualityButton.bottomAnchor.constraint(equalTo: playerView.bottomAnchor, constant: -16)
-        ])
-    }
-    
-    @objc func showQualityOptions() {
-        let alert = UIAlertController(title: "Select Quality", message: nil, preferredStyle: .actionSheet)
-        for (resolution, url) in qualities {
-            alert.addAction(UIAlertAction(title: resolution, style: .default, handler: { _ in
-                self.changeQuality(url: url)
+    @objc private func changeQuality() {
+        let alert = UIAlertController(title: "Change Quality", message: "Select quality", preferredStyle: .actionSheet)
+
+        for (title, bitrate) in availableQualities {
+            alert.addAction(UIAlertAction(title: title, style: .default, handler: { _ in
+                self.setQuality(bitrate: bitrate)
             }))
         }
-        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-        present(alert, animated: true, completion: nil)
-    }
-    
-    func changeQuality(url: URL) {
-        guard let player = customPlayerVC?.player else { return }
-        customPlayerVC?.currentTime = player.currentTime()
-        customPlayerVC?.player?.pause()
-        addPlayerView(url: url, resumeTime: customPlayerVC?.currentTime ?? .zero)
-    }
-}
 
-//MARK: - Table View -
-extension ViewController: UITableViewDataSource, UITableViewDelegate {
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        self.present(alert, animated: true)
+    }
+
+    private func setQuality(bitrate: Double) {
+        player.currentItem?.preferredPeakBitRate = bitrate
+    }
+
+    @objc private func checkFullscreen() {
+        let isFullscreen = playerViewController.isBeingPresented
+        qualityButton.isHidden = isFullscreen
+    }
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        videoUrls.count
+        return videoUrls.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "VideoPlayerTVC", for: indexPath) as! VideoPlayerTVC
-        cell.lbl.text = "Video №\(indexPath.row + 1)"
-        cell.lbl.textColor = (indexPath.row == playingIndex) ? .blue : .black
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Video \(indexPath.row + 1)"
+        cell.textLabel?.textColor = .black
+
+        cell.backgroundColor = .white
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        playVideo(at: indexPath.row)
+        playVideo(url: videoUrls[indexPath.row])
     }
 }
